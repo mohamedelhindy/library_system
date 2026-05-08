420f42907945d1702b4c3b0a6b44e365a961669b
mohamedelhindy <mohamed.hiendy@gmail.com>
2026-05-08 15:37:54 +0300

    feat: Implement Dashboard page

--- pages/Dashboard.java
+++ pages/Dashboard.java
@@ -2,19 +2,12 @@ package library_system.pages;
 
 import java.awt.*;
 import javax.swing.*;
-
-import library_system.components.StyledButton;
-import library_system.utils.Navigator;
+import library_system.components.Navbar;
 
 public class Dashboard extends JPanel {
 
     private JLabel title;
     private JLabel welcomeLabel;
-
-    private StyledButton booksBtn;
-    private StyledButton historyBtn;
-    private StyledButton profileBtn;
-    private StyledButton logoutBtn;
 
     public Dashboard() {
         initializeComponents();
@@ -25,16 +18,9 @@ public class Dashboard extends JPanel {
     private void initializeComponents() {
         title = new JLabel("Dashboard");
         welcomeLabel = new JLabel("Welcome back!");
-
-        booksBtn = new StyledButton("Books");
-        historyBtn = new StyledButton("History");
-        profileBtn = new StyledButton("Profile");
-        logoutBtn = new StyledButton("Logout");
     }
 
     private void styleComponents() {
-        setBackground(Color.BLACK);
-
         title.setFont(new Font("Arial", Font.BOLD, 60));
         title.setForeground(Color.WHITE);
         title.setAlignmentX(CENTER_ALIGNMENT);
@@ -42,42 +28,25 @@ public class Dashboard extends JPanel {
         welcomeLabel.setFont(new Font("Arial", Font.PLAIN, 20));
         welcomeLabel.setForeground(Color.LIGHT_GRAY);
         welcomeLabel.setAlignmentX(CENTER_ALIGNMENT);
-
-        Dimension btnSize = new Dimension(200, 50);
-        booksBtn.setMaximumSize(btnSize);
-        historyBtn.setMaximumSize(btnSize);
-        profileBtn.setMaximumSize(btnSize);
-        logoutBtn.setMaximumSize(btnSize);
-
-        booksBtn.setAlignmentX(CENTER_ALIGNMENT);
-        historyBtn.setAlignmentX(CENTER_ALIGNMENT);
-        profileBtn.setAlignmentX(CENTER_ALIGNMENT);
-        logoutBtn.setAlignmentX(CENTER_ALIGNMENT);
-
-        logoutBtn.setBackground(new Color(180, 30, 30));
     }
 
     private void layoutComponents() {
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setLayout(new BorderLayout());
 
-        add(Box.createVerticalGlue());
-        add(title);
-        add(Box.createVerticalStrut(10));
-        add(welcomeLabel);
-        add(Box.createVerticalStrut(50));
-        add(booksBtn);
-        add(Box.createVerticalStrut(15));
-        add(historyBtn);
-        add(Box.createVerticalStrut(15));
-        add(profileBtn);
-        add(Box.createVerticalStrut(30));
-        add(logoutBtn);
-        add(Box.createVerticalGlue());
+        add(new Navbar(), BorderLayout.WEST);
 
-        addListeners();
-    }
+        JPanel content = new JPanel();
+        content.setBackground(Color.BLACK);
+        content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
 
-    private void addListeners() {
-        logoutBtn.addActionListener(e -> Navigator.navigateTo(new LoginPage()));
+        content.add(Box.createVerticalGlue());
+        content.add(title);
+
+        content.add(Box.createVerticalStrut(10));
+        
+        content.add(welcomeLabel);
+        content.add(Box.createVerticalGlue());
+
+        add(content, BorderLayout.CENTER);
     }
 }
